9b333b78233d248eb475f486e35a4bdd2c8fec31
vtmc-cofice <vladimir.tomic0209@gmail.com>
2020-03-26 01:44:40 +0100

    resen problem navbar-Navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 import "./layout.css"
 
-import Navbar from "./Globals/navbar"
+import Navbar from "./Globals/Navbar"
 const Layout = ({ children }) => (
   <>
     <Navbar />
